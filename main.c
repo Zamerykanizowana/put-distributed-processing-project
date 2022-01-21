be71e1e1e54193db993fc0979587169b5166c8a2
Adam Olech <nddr89@gmail.com>
2022-01-21 01:29:54 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <mpi.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define ROOT 0
+#define MSG_TAG 100
+
+int main(int argc, char **argv) {
+	int tid, size;
+	MPI_Status status;
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &tid);
+}
