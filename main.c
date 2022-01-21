e7074ee01c4f317193485d0914eec91ab99fc567
Adam Olech <nddr89@gmail.com>
2022-01-21 01:46:15 +0100

    main: log and finalize

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "log.h"
 #define ROOT 0
 #define MSG_TAG 100
 
@@ -12,4 +13,8 @@ int main(int argc, char **argv) {
 
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &tid);
+
+	log_info("id %d/%d", tid, size);
+
+	MPI_Finalize();
 }
