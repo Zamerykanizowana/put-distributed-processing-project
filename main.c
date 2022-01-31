c08cc22b9d3e9132d4076e16252ba5e0c88cd37a
Adam Olech <nddr89@gmail.com>
2022-02-01 00:00:04 +0100

    Some initial doodles

--- main.c
+++ main.c
@@ -1,20 +1,38 @@
 #include <mpi.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "log.h"
+#include "tourist.h"
 #define ROOT 0
 #define MSG_TAG 100
 
+void sigint_handler(int num) {
+	log_info("%d is closing...", T.rank);
+
+	MPI_Finalize();
+}
+
+void main_event_loop() {
+	int i = 0;
+
+	while (1) {
+		log_info("%d is loopin' and sleepin': %d", T.rank, i);
+		sleep(1);
+		i++;
+	}
+}
+
 int main(int argc, char **argv) {
-	int tid, size;
-	MPI_Status status;
+	signal(SIGINT, sigint_handler);
 
 	MPI_Init(&argc, &argv);
 
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &tid);
+	MPI_Comm_size(MPI_COMM_WORLD, &T.size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &T.rank);
 
-	log_info("id %d/%d", tid, size);
+	log_info("id %d/%d", T.size, T.rank);
 
-	MPI_Finalize();
+	main_event_loop();
 }
---
+++ tourist.h
@@ -0,0 +1,10 @@
+#ifndef GATES_TOURIST_H
+#define GATES_TOURIST_H
+
+struct tourist {
+	int rank;
+	int size;
+	int clk;
+} T;
+
+#endif
