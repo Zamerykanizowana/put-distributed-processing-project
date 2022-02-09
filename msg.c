5d2c90f0e87e954b8d45a199c0fdc7c386f376ce
Adam Olech <nddr89@gmail.com>
2022-02-10 00:37:27 +0100

    msg: implement incoming_event_happened_before

--- msg.c
+++ msg.c
@@ -1,1 +1,11 @@
 #include "msg.h"
+#include "tourist.h"
+
+// Check if sender wins.
+// In case of timestamp conflict, lower rank wins.
+int incoming_event_happened_before(int incoming_clk, int incoming_rank) {
+	return (T.clk > incoming_clk || 
+			(T.clk == incoming_clk && 
+			 incoming_rank < T.rank)
+	       );
+}
