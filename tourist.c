c1e09d237570e7df3bfbf6cc927402806c23af48
Adam Olech <nddr89@gmail.com>
2022-02-14 01:49:06 +0100

    tourist: add a safe guard

--- tourist.c
+++ tourist.c
@@ -1,4 +1,5 @@
 #include "tourist.h"
+#include "log.h"
 
 tourist T;
 
@@ -11,5 +12,10 @@ void T_enter_store(int tourist) {
 
 void T_leave_store(int tourist) {
 	T.free_store_slots++;
+
+	if (T.free_store_slots > T.total_store_slots) {
+		log_error("Free store slots (%d) exceeded total store slots (%d)",
+				T.free_store_slots, T.total_store_slots);
+	}
 	T.res[tourist].store_claimed = 0;
 }
