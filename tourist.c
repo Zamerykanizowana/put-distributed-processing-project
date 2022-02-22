38ec86941e232241845c40701d67fa6fddd76fbb
Adam Olech <nddr89@gmail.com>
2022-02-22 02:41:19 +0100

    I may have ensured liveness...

--- tourist.c
+++ tourist.c
@@ -23,11 +23,13 @@ void T_enter_store(int tourist) {
 }
 
 void T_leave_store(int tourist) {
-	T.free_store_slots++;
+	if (T.res[tourist].store_claimed == 1) {
+		T.res[tourist].store_claimed = 0;
+		T.free_store_slots++;
+	}
 
 	if (T.free_store_slots > T.total_store_slots) {
 		log_error("Free store slots (%d) exceeded total store slots (%d)",
 				T.free_store_slots, T.total_store_slots);
 	}
-	T.res[tourist].store_claimed = 0;
 }
