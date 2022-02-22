5d12cffe75c8a9b00dd8e5d257a9d45591613d9d
Adam Olech <nddr89@gmail.com>
2022-02-22 21:10:57 +0100

    run.sh: add parameter to specify number of processes to start

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 BIN=main
+NPROC="${1:-4}"
+
+echo "Running with $NPROC processes!"
+
 mpicc *.c -o $BIN -Wall -DLOG_USE_COLOR \
-    && mpirun --mca opal_warn_on_missing_libcuda 0 ./$BIN
+    && mpirun -np $NPROC --oversubscribe --mca opal_warn_on_missing_libcuda 0 ./$BIN
